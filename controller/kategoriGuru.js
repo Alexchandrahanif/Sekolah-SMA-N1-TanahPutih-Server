a36b99b30d56d2eb4da305c2239d56c22804491d
Alexchandrahanif <alexchandra1998@gmail.com>
2022-12-12 23:17:30 +0700

    Kategori Guru selesai

--- controller/kategoriGuru.js
+++ controller/kategoriGuru.js
@@ -15,7 +15,6 @@ class Controller {
   }
 
   // Melihat data kategori guru berdasarkan ID
-
   static async getKategoriGuru(req, res, next) {
     try {
       const { id } = req.params;
@@ -27,6 +26,74 @@ class Controller {
       res.status(200).json({
         statusCode: 200,
         data: dataGuru,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  // Menambahkan Kategori Guru Baru
+  static async addKategoriGuru(req, res, next) {
+    try {
+      const { nama } = req.body;
+      const dataKategori = await KategoriGuru.create({
+        nama,
+      });
+      res.status(201).json({
+        statusCode: 201,
+        message: `Berhasil menambahkan kategori guru baru`,
+        data: dataKategori,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  // Memperbaharui data kategori guru
+  static async editKategoriGuru(req, res, next) {
+    try {
+      const { nama } = req.body;
+      const { id } = req.params;
+      const dataGuru = await KategoriGuru.findByPk(id);
+      if (!dataGuru) {
+        throw { name: "Data Kategori Guru Not Found", id: id };
+      }
+      const dataKategori = await KategoriGuru.update(
+        {
+          nama,
+        },
+        {
+          where: {
+            id,
+          },
+        }
+      );
+      res.status(200).json({
+        statusCode: 200,
+        message: `Kategori Guru dengan id ${id} berhasil diperbaharui`,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  // Menghapus data Kategori guru
+  static async deleteKategoriGuru(req, res, next) {
+    try {
+      const { id } = req.params;
+      const dataGuru = await KategoriGuru.findByPk(id);
+
+      if (!dataGuru) {
+        throw { name: "Data Kategori Guru Not Found", id: id };
+      }
+      const dataKategori = await KategoriGuru.destroy({
+        where: {
+          id,
+        },
+      });
+      res.status(200).json({
+        statusCode: 200,
+        message: `Kategori Guru dengan id ${id} berhasil dihapus`,
       });
     } catch (error) {
       next(error);
--- router/kategoriGuru.js
+++ router/kategoriGuru.js
@@ -3,5 +3,8 @@ const kategoriGuruRouter = require("express").Router();
 
 kategoriGuruRouter.get("/", Controller.getKategoriGurus);
 kategoriGuruRouter.get("/:id", Controller.getKategoriGuru);
+kategoriGuruRouter.post("/", Controller.addKategoriGuru);
+kategoriGuruRouter.put("/:id", Controller.editKategoriGuru);
+kategoriGuruRouter.delete("/:id", Controller.deleteKategoriGuru);
 
 module.exports = kategoriGuruRouter;
